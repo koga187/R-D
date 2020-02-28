8ba98f360ced3e952f5ccb98dcf7dbcc18493128
Bruno Koga <brunokoga187@gmail.com>
2020-02-28 12:25:23 -0300

    Adicionada dependencia que irá ajudar a gerar os códigos dos modelos rotas para Api REST

--- lumen/bootstrap/app.php
+++ lumen/bootstrap/app.php
@@ -21,9 +21,9 @@ $app = new Laravel\Lumen\Application(
     dirname(__DIR__)
 );
 
-// $app->withFacades();
+$app->withFacades();
 
-// $app->withEloquent();
+$app->withEloquent();
 
 /*
 |--------------------------------------------------------------------------
@@ -89,7 +89,7 @@ $app->configure('app');
 |
 */
 
-// $app->register(App\Providers\AppServiceProvider::class);
+$app->register(App\Providers\AppServiceProvider::class);
 // $app->register(App\Providers\AuthServiceProvider::class);
 // $app->register(App\Providers\EventServiceProvider::class);
 
